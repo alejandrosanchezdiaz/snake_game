291793dffe62c49a7463250a65c7ed578489c7c5
Alex <alesandi.uk@gmail.com>
2020-09-29 16:45:28 +0100

    V1_snake-class

--- snake/src/Snake_V1/SnakeGame.java
+++ snake/src/Snake_V1/SnakeGame.java
@@ -7,13 +7,20 @@ import java.awt.event.KeyListener;
 
 public class SnakeGame extends Applet implements Runnable, KeyListener {
 
+    Graphics gfx;
+    Image img;
+
     public void init(){
         this.resize(400, 400);
+        img = createImage(400,400);
+        gfx = img.getGraphics();
     }
 
     public void paint(Graphics g){
-        g.setColor(Color.black);
-        g.fillRect(0,0,400,400);
+        gfx.setColor(Color.black);
+        gfx.fillRect(0,0,400,400);
+
+        g.drawImage(img,0,0,null);
     }
 
     public void update(Graphics g){
@@ -29,7 +36,18 @@ public class SnakeGame extends Applet implements Runnable, KeyListener {
     }
 
     public void keyTyped(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP){
 
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+
+        }
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+
+        }
     }
 
     public void keyPressed(KeyEvent e) {
