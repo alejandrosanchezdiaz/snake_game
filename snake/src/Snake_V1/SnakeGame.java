103658c6f25cd8f88e2b25d0177c6ac3f58ddad5
Alex <alesandi.uk@gmail.com>
2020-09-30 10:16:54 +0100

    V1-plug

--- snake/src/Snake_V1/SnakeGame.java
+++ snake/src/Snake_V1/SnakeGame.java
@@ -9,16 +9,24 @@ public class SnakeGame extends Applet implements Runnable, KeyListener {
 
     Graphics gfx;
     Image img;
+    Thread thread;
+    Snake snake;
 
     public void init(){
         this.resize(400, 400);
         img = createImage(400,400);
         gfx = img.getGraphics();
+        snake = new Snake();
+        thread = new Thread(this);
+        thread.start();
     }
 
     public void paint(Graphics g){
         gfx.setColor(Color.black);
         gfx.fillRect(0,0,400,400);
+        snake.draw(gfx);
+
+
 
         g.drawImage(img,0,0,null);
     }
@@ -32,7 +40,16 @@ public class SnakeGame extends Applet implements Runnable, KeyListener {
     }
 
     public void run() {
+        for(;;){
 
+            this.repaint();
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     public void keyTyped(KeyEvent e) {
